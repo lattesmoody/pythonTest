14329c16dc3d09c797e709b3c5d22c9ce9a27381
lattesmoody <lattesmoody@gmail.com>
2024-03-28 19:52:50 +0900

    240328 학습내용저장2

--- _pythonStudy/_2024/_240328/240328 학습 내용.sql
+++ _pythonStudy/_2024/_240328/240328 학습 내용.sql
@@ -1,3 +1,19 @@
 [데이터 그룹화]
 
 [데이터 집계]
+* 중요 집계 매서드
+1. first,last
+2. nth
+3. ohlc
+
+* agg() 메서드: 사용자 정의 집계 함수를 그룹화 집계 함수로 사용.
+예) grouped[["data1", "data2"]].agg(peak_to_peak)
+
+* grouped_pct.agg("mean")
+=> 여기서 "mean"은 문자열 "mean"이 아니라, 
+평균값을 구하는 mean 메서드를 의미한다.
+
+
+[apply: 다목적 데이터 집계]
+
+tips.groupby("smoker").apply(top)
