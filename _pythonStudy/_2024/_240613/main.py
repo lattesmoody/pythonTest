97937b42338d88a53d08728861427e8aeb52c434
lattesmoody <lattesmoody@gmail.com>
2024-06-13 03:02:11 +0900

    apscheduler logging WARN setting.

--- _pythonStudy/_2024/_240613/main.py
+++ _pythonStudy/_2024/_240613/main.py
@@ -8,6 +8,7 @@ import logging
 # 로깅 설정
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+logging.getLogger('apscheduler').setLevel(logging.WARN)
 
 scheduler = AsyncIOScheduler()
 
